328fe579e3526cb3055b349d7a4a151fffce48cb
Érick <eierickdasilva@gmail.com>
2024-11-03 10:23:30 -0300

    up

---
+++ src/app/components/nutrition-dialog/nutrition-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-nutrition-dialog',
+  standalone: true,
+  imports: [
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    MatButton,
+    MatDialogClose,
+  ],
+  templateUrl: './nutrition-dialog.component.html',
+  styleUrl: './nutrition-dialog.component.css'
+})
+export class NutritionDialogComponent {
+
+}
--- src/app/pages/alunos/alunos.component.ts
+++ src/app/pages/alunos/alunos.component.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,8 @@ import { register } from 'swiper/element/bundle';
 import { StorageService } from '../../services/storage.service';
 import { spreadsheetsService } from '../../services/spreadsheets.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NutritionDialogComponent } from '../../components/nutrition-dialog/nutrition-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 register();
 
@@ -26,6 +28,8 @@ register();
 export class AlunosComponent {
   phrases = ['Alimente seu corpo, cultive sua saúde.', 'Uma boa alimentação é o primeiro passo para uma vida mais saudável.', 'Coma bem, viva melhor.', 'Seu corpo agradece cada alimento saudável.', 'A comida é o combustível do seu corpo. Abasteça-o com o melhor!', 'A saúde começa no prato.', 'Alimente seus sonhos com uma dieta balanceada.', 'Uma dieta equilibrada é a receita para uma vida feliz e saudável.', 'Invista em sua saúde, invista em uma boa alimentação.',
   'Cada garfada é uma oportunidade para nutrir seu corpo.', 'A alimentação saudável não é uma dieta, é um estilo de vida.', 'Você é o que você come. Escolha bem!', 'A comida é o nosso melhor remédio.', 'Cozinhar com amor é nutrir a alma.', 'Uma alimentação saudável é um presente para o seu futuro.','A comida é a nossa primeira medicina.', 'A alimentação saudável é a base para uma vida ativa e produtiva.', 'Ame seu corpo, alimente-o com carinho.', 'O que você come hoje define como você se sentirá amanhã.', 'A comida não é apenas combustível, é uma experiência.', 'A alimentação é uma forma de amor próprio.', 'Escolha alimentos que alimentem seu corpo e seu espírito.', 'A comida conecta as pessoas e a natureza.', 'A alimentação saudável é um ato de amor por você mesmo.', 'A alimentação saudável é um investimento a longo prazo.', 'Cada escolha alimentar é uma oportunidade para crescer.','A comida é a nossa linguagem universal. Fale a linguagem da saúde.', 'A natureza nos oferece a melhor farmácia: os alimentos naturais.', 'Um corpo saudável é a nossa maior riqueza.', 'A comida é a arte de nutrir o corpo e a alma.', 'A felicidade se encontra também no prato.', 'A alimentação saudável é um hábito, não uma obrigação.', 'Cozinhar é um ato de amor e cuidado consigo mesmo.', 'A comida é a nossa primeira medicina preventiva.', 'Uma boa digestão é a base de uma boa saúde.', 'A alimentação saudável nos conecta com a natureza e com nós mesmos.', 'Escolha alimentos que te deixem leve e energizado.', 'A comida é celebração da vida.', 'A alimentação saudável é um estilo de vida que contagia.', 'Um corpo bem nutrido é mais resistente a doenças.', 'A comida é a nossa primeira casa.', 'A alimentação saudável é um ato de gratidão à vida.', 'Coma devagar e saboreie cada mordida.', 'A comida é a nossa melhor companhia.', 'A alimentação saudável é um investimento no futuro.', 'A comida nos conecta com nossas raízes.', 'A alimentação saudável é um ato de amor pela vida.', 'A comida é a nossa primeira paixão.', 'A alimentação saudável é um estilo de vida que transforma.', 'Um corpo saudável é uma mente saudável.'];
+
+  readonly dialog = inject(MatDialog);
 
   phrase!: string;
   data: any;
@@ -112,6 +116,10 @@ export class AlunosComponent {
   private loadCard(): void {
     this.clearMenu();
     for (let i = 0; i <= this.data.length; i++) this.menu.push(new SheetsData(this.days[i], this.data[0][i], this.data[1][i]));
+  }
+
+  public openNutritionalDialog() {
+    this.dialog.open(NutritionDialogComponent);
   }
   
   private clearMenu(): void {
